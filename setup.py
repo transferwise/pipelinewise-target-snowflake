5162a20b035ab7448352e165530bf0da44782831
Samira El Aabidi <samira.elaabidi@transferwise.com>
2019-11-04 11:17:30 +0200

    upgrading snowflake-connector-python

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(name="pipelinewise-target-snowflake",
       install_requires=[
           'idna==2.7',
           'singer-python==5.1.1',
-          'snowflake-connector-python==2.0.1',
+          'snowflake-connector-python==2.0.3',
           'boto3==1.9.33',
           'inflection==0.3.1',
           'joblib==0.13.2'
