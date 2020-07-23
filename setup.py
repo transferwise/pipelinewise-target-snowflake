f912b9efee5989f6263314bd448444fcb0ba27e7
Henri Blancke <blanckehenri@gmail.com>
2020-07-23 07:44:34 -0400

    Support ACL for S3 uploads (#89)

--- setup.py
+++ setup.py
@@ -32,7 +32,8 @@ setup(name="pipelinewise-target-snowflake",
           "test": [
               "nose==1.3.7",
               "mock==3.0.5",
-              "pylint==2.4.2"
+              "pylint==2.4.2",
+              "python-dotenv==0.14.0"
           ]
       },
       entry_points="""
