151bc51e7c71e5909b346b8a9dd41be7c4d1eb22
Samira El Aabidi <54845154+Samira-El@users.noreply.github.com>
2022-03-29 12:18:16 +0300

    Drop Support for Python 3.6 and add 3.9 (#259)

--- setup.py
+++ setup.py
@@ -15,9 +15,9 @@ setup(name="pipelinewise-target-snowflake",
       classifiers=[
           'License :: OSI Approved :: Apache Software License',
           'Programming Language :: Python :: 3 :: Only',
-          'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
       ],
       py_modules=["target_snowflake"],
       install_requires=[
